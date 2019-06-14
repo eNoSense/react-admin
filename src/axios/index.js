1002008551f2354527b674f4b88604cdfbf9be1d
guakun <2306831967@qq.com>
2019-06-14 18:01:45 +0800

    table - 7-6

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,4 +1,6 @@
 import JsonP from "jsonp";
+import axios from "axios";
+import { Modal } from "antd";
 export default class Axios {
   static jsonp(options) {
     return new Promise((resolve, reject) => {
@@ -16,6 +18,43 @@ export default class Axios {
           }
         }
       );
+    });
+  }
+
+  static ajax(options) {
+    let loading;
+    if (options.data && options.data.isShowLoading !== false) {
+      loading = document.querySelector('#ajaxLoading')
+      loading.style.display = 'block'
+    }
+    const baseURL = "https://easy-mock.com/mock/5d0342af599408638e6ad79c/raapi";
+    return new Promise((resolve, reject) => {
+      axios({
+        url: options.url,
+        method: "GET",
+        baseURL,
+        timeout: 5000,
+        params: (options.data && options.data.params) || ""
+      }).then(responseText => {
+        if (options.data && options.data.isShowLoading !== false) {
+          loading = document.querySelector('#ajaxLoading')
+          loading.style.display = 'none'
+        }
+        const {data, status} = responseText
+        if (status === 200) {
+          // 业务
+          if (data.code === 0) {
+            resolve(data)
+          } else {
+            Modal.info({
+              title: "提示",
+              content: data.msg
+            })
+          }
+        } else {
+          reject(data)
+        }
+      });
     });
   }
 }
--- src/pages/table/basic/index.js
+++ src/pages/table/basic/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import { Card, Table } from "antd";
+import { Card, Table, Modal, Button, message } from "antd";
+import axios from "./../../../axios";
 import "./index.less";
 
 export default class BasicTable extends Component {
-  state = {};
+  state = {
+    dataSource2: []
+  };
   componentDidMount() {
     const dataSource = [
       {
+        key: 1,
         id: 0,
         username: "gua1",
         gender: "1",
@@ -17,6 +21,7 @@ export default class BasicTable extends Component {
         time: "09:00"
       },
       {
+        key: 2,
         id: 1,
         username: "gua1",
         gender: "1",
@@ -28,27 +33,151 @@ export default class BasicTable extends Component {
       }
     ];
     this.setState({ dataSource });
+    this.request();
   }
+
+  request = () => {
+    axios
+      .ajax({
+        url: "/table/list",
+        // url: "test/exception",
+        data: { params: { page: 1 } }
+      })
+      .then(response => {
+        this.setState({
+          dataSource2: response.data.items,
+          selectedRowKeys: [],
+          selectedRowIds: null
+        });
+      });
+  };
+
+  onRowClick = (record, index) => {
+    let selectedKey = [index];
+    Modal.info({
+      title: "信息",
+      content: `用户名${record.username}, 的爱好是 ${record.hobby}`
+    });
+    this.setState({ selectedRowKeys: selectedKey, selectedItem: record });
+  };
+
+  handleDelete = () => {
+    const ids = this.state.selectedRowIds || [];
+    Modal.confirm({
+      title: "确认",
+      content: `您确定要删除这些数据嘛?${ids.join(",")}`,
+      onOk: () => {
+        message.success("删除成功");
+        this.request()
+      }
+    });
+  };
 
   render() {
     const columns = [
       { title: "id", dataIndex: "id" },
       { title: "用户名", dataIndex: "username" },
-      { title: "姓名", dataIndex: "gender" },
-      { title: "状态", dataIndex: "state" },
-      { title: "爱好", dataIndex: "hobby" },
+      {
+        title: "性别",
+        dataIndex: "gender",
+        render(gender) {
+          return gender === 1 ? "男" : "女";
+        }
+      },
+      {
+        title: "状态",
+        dataIndex: "state",
+        render(state) {
+          let config = {
+            1: "瓜一个",
+            2: "瓜两个",
+            3: "瓜三个",
+            4: "瓜四个",
+            5: "瓜五个"
+          };
+          return config[state];
+        }
+      },
+      {
+        title: "爱好",
+        dataIndex: "hobby",
+        render(hobby) {
+          let config = {
+            1: "爱瓜一",
+            2: "爱瓜两",
+            3: "爱瓜三",
+            4: "爱瓜四",
+            5: "爱瓜五"
+          };
+          return config[hobby];
+        }
+      },
       { title: "生日", dataIndex: "birthday" },
       { title: "地址", dataIndex: "address" },
       { title: "起床时间", dataIndex: "time" }
     ];
+
+    const paginationConfig = {
+      current: 1,
+      pageSize: 10,
+      total: 30
+    };
+    const { selectedRowKeys } = this.state;
+
+    // const rowSelectionConfig = {
+    //   type: "radio",
+    //   selectedRowKeys
+    // };
+
+    const rowCheckSelectionConfig = {
+      type: "checkbox",
+      selectedRowKeys,
+      onChange: (selectedRowKeys, selectedRows) => {
+        const selectedRowIds = selectedRows.map(row => {
+          return row.id;
+        });
+        this.setState({
+          selectedRowKeys,
+          selectedRowIds
+        });
+      }
+    };
+
     return (
       <div className="demo-wrapper">
-        <Card title="表格">
+        <Card title="基础表格">
           <Table
             bordered
             pagination={false}
             columns={columns}
             dataSource={this.state.dataSource}
+          />
+        </Card>
+
+        <Card title="Mock 动态渲染表格">
+          <Table
+            pagination={paginationConfig}
+            columns={columns}
+            dataSource={this.state.dataSource2}
+          />
+        </Card>
+
+        <Card title="Mock 单选">
+          <div>
+            <Button onClick={this.handleDelete}>删除</Button>
+          </div>
+          <Table
+            rowSelection={rowCheckSelectionConfig}
+            pagination={paginationConfig}
+            columns={columns}
+            // onRow={(record, index) => {
+            //   return {
+            //     onClick: () => {
+            //       this.onRowClick(record, index);
+            //     }
+            //   };
+            // }}
+            dataSource={this.state.dataSource2}
           />
         </Card>
       </div>
